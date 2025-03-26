5677ff819106d8b437256cc6d28be00bdf615fb1
RiyazShaik-158 <riyaz.shaik.1481@gmail.com>
2025-03-26 05:35:21 +0530

    feat(router fix): separating routes for easier usage

--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -1,18 +1,11 @@
-import Login from "../components/Login";
 import Navbar from "../components/Navbar";
-import { Routes, Route } from "react-router-dom";
-import SignUp from "../components/SignUp";
-import Home from "../components/Home";
+import Router from "../routes/Router";
 
 const Layout = () => {
   return (
     <div className="w-screen h-screen text-black font-sans">
       <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<SignUp />} />
-      </Routes>
+      <Router />
     </div>
   );
 };
---
+++ src/routes/Router.tsx
@@ -0,0 +1,16 @@
+import { Route, Routes } from "react-router-dom";
+import Home from "../components/Home";
+import Login from "../components/Login";
+import SignUp from "../components/SignUp";
+
+const Router = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/signup" element={<SignUp />} />
+    </Routes>
+  );
+};
+
+export default Router;
