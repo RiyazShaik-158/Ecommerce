f225e97615a6e1a6bdba1ed7657e2b3ccfcdc6f0
RiyazShaik-158 <riyaz.shaik.1481@gmail.com>
2025-03-30 23:54:14 +0530

    feat(basic logout): implemented basic logout for temp purpose

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,9 +1,9 @@
 import { Link } from "react-router-dom";
 import { useAuth } from "../context/Auth";
+import { Popover, PopoverButton, PopoverPanel } from "@headlessui/react";
 
 const Navbar = () => {
-  const { user } = useAuth();
-  console.log("user in navbar is", user);
+  const { user, logout } = useAuth();
   return (
     <div className="w-full h-16 bg-[#61bf1a] flex justify-between items-center px-[20px]">
       <Link to={"/"}>
@@ -16,7 +16,25 @@ const Navbar = () => {
           className="w-[500px] outline-none border-2 border-black p-1.5 rounded-lg shadow-cyan-200"
         />
         {user ? (
-          <h4>{`Hello ${user}`}</h4>
+          <>
+            <h4>{`Hello ${user}`}</h4>
+            <Popover className="group">
+              <PopoverButton className="flex items-center gap-2">
+                Solutions
+              </PopoverButton>
+              <PopoverPanel anchor="bottom" className="flex flex-col">
+                {/* <a href="/insights">Insights</a>
+        <a href="/automations">Automations</a>
+        <a href="/reports">Reports</a> */}
+                <button
+                  onClick={logout}
+                  className="p-1 px-2 rounded text-red-500 border-2 border-red-500 bg-white"
+                >
+                  Logout
+                </button>
+              </PopoverPanel>
+            </Popover>
+          </>
         ) : (
           <Link to={"/login"}>
             <h4 className="text-black">Login</h4>
