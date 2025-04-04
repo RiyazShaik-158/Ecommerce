5a9f6e56f8400a238df38289e5ed9023a60ea47d
RiyazShaik-158 <riyaz.shaik.1481@gmail.com>
2025-04-04 08:58:04 +0530

    fix(remove delivery): will catch up later on delivery location

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -10,7 +10,7 @@ const Navbar = () => {
         <h4 className="text-black">Ecommerce site</h4>
       </Link>
       <div className="flex items-center justify-between w-3/4">
-        <h4>Delivery Location</h4>
+        <div></div>
         <input
           placeholder="Search..."
           className="w-[500px] outline-none border-2 border-black p-1.5 rounded-lg shadow-cyan-200"
