3864a627583d80c3ca6311f8bf6951fad6496ec1
RiyazShaik-158 <riyaz.shaik.1481@gmail.com>
2025-04-17 06:33:42 +0530

    feat(font and navbar changes): changes made in navbar for login page and css added for changing font style

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,44 +4,54 @@ import { Popover, PopoverButton, PopoverPanel } from "@headlessui/react";
 
 const Navbar = () => {
   const { user, logout } = useAuth();
+  const address = window.location.pathname;
+
   return (
     <div className="w-full h-16 bg-[#61bf1a] flex justify-between items-center px-[20px]">
       <Link to={"/"}>
         <h4 className="text-black">Ecommerce site</h4>
       </Link>
-      <div className="flex items-center justify-between w-3/4">
-        <div></div>
-        <input
-          placeholder="Search..."
-          className="w-[500px] outline-none border-2 border-black p-1.5 rounded-lg shadow-cyan-200"
-        />
-        {user ? (
-          <Popover
-            as="div"
-            className="group !m-0 !p-0 !border-0 !bg-white !rounded-xl"
-          >
-            <PopoverButton
-              as="button"
-              className="flex items-center gap-2 font-semibold !bg-white !rounded !text-blue-500 !p-2 !px-3"
-            >
-              {`Hello ${String(user)}`}
-            </PopoverButton>
-            <PopoverPanel as={"div"} anchor="bottom" className="flex flex-col">
-              <button
-                onClick={logout}
-                className="p-1 px-2 rounded text-red-500 border-2 border-red-500 bg-white w-[100px]"
+      {address.includes("/login") || address.includes("/signup") ? null : (
+        <>
+          <div className="flex items-center justify-between w-3/4">
+            <div></div>
+            <input
+              placeholder="Search..."
+              className="w-[500px] outline-none border-2 border-black p-1.5 rounded-lg shadow-cyan-200"
+            />
+            {user ? (
+              <Popover
+                as="div"
+                className="group !m-0 !p-0 !border-0 !bg-white !rounded-xl"
               >
-                Logout
-              </button>
-            </PopoverPanel>
-          </Popover>
-        ) : (
-          <Link to={"/login"}>
-            <h4 className="text-black">Login</h4>
-          </Link>
-        )}
-      </div>
-      <h4>Cart</h4>
+                <PopoverButton
+                  as="button"
+                  className="flex items-center gap-2 font-semibold !bg-white !rounded !text-blue-500 !p-2 !px-3"
+                >
+                  {`Hello ${String(user)}`}
+                </PopoverButton>
+                <PopoverPanel
+                  as={"div"}
+                  anchor="bottom"
+                  className="flex flex-col"
+                >
+                  <button
+                    onClick={logout}
+                    className="p-1 px-2 rounded text-red-500 border-2 border-red-500 bg-white w-[100px]"
+                  >
+                    Logout
+                  </button>
+                </PopoverPanel>
+              </Popover>
+            ) : (
+              <Link to={"/login"}>
+                <h4 className="text-black">Login</h4>
+              </Link>
+            )}
+          </div>
+          <h4>Cart</h4>
+        </>
+      )}
     </div>
   );
 };
