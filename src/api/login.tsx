c63fa2ca8319091131a1336c4abd69fc038b26c7
RiyazShaik-158 <riyaz.shaik.1481@gmail.com>
2025-06-14 20:30:37 +0530

    feat(update login url): update login url

--- src/api/login.tsx
+++ src/api/login.tsx
@@ -2,6 +2,6 @@ import axios from "axios";
 import { backend_url } from "../constants/constants";
 
 export const loginUser = async (loginPayload: Record<string, unknown>) => {
-  const response = await axios.post(`${backend_url}/users/auth`, loginPayload);
+  const response = await axios.post(`${backend_url}/auth/login`, loginPayload);
   return response.data;
 };
